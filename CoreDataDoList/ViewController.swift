2660f8e8c7af92472715a51ee20d879359fd9d8d
Kaan Yeyrek <kaanyeyrek@icloud.com>
2022-08-09 03:29:04 +0300

    Initial Commit

---
+++ CoreDataDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDoList
+//
+//  Created by Kaan Yeyrek on 8/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
